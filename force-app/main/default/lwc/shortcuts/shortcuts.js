4bf9e0706e2bdaff1fe38be5ec0633e1cb9087b3
[Ben Learn] <blearn5241@gmail.com>
2020-12-18 19:27:53 -0500

    updated to follow best practices by using const

--- force-app/main/default/lwc/shortcuts/shortcuts.js
+++ force-app/main/default/lwc/shortcuts/shortcuts.js
@@ -3,27 +3,29 @@ import { refreshApex } from '@salesforce/apex'
 import { getRecord } from 'lightning/uiRecordApi'
 import getClicksId from '@salesforce/apex/ShortcutButtonsController.getClicksId'
 
+const SHORTCUTS = [
+    {
+        Name: "Setup",
+        Icon: "utility:setup",
+        URL: "/lightning/setup/SetupOneHome/home"
+    },
+    {
+        Name: "Object Manager",
+        Icon: "utility:database",
+        URL: "/lightning/setup/ObjectManager/home"
+    },
+    {
+        Name: "Developer Console",
+        Icon: "utility:apex",
+        URL: "/_ui/common/apex/debug/ApexCSIPage"
+    }
+]
+
 export default class Shortcuts extends LightningElement {
     error = undefined
     csId = ''
     recordExists = false
-    shortcuts = [
-        {
-            Name: "Setup",
-            Icon: "utility:setup",
-            URL: "/lightning/setup/SetupOneHome/home"
-        },
-        {
-            Name: "Object Manager",
-            Icon: "utility:database",
-            URL: "/lightning/setup/ObjectManager/home"
-        },
-        {
-            Name: "Developer Console",
-            Icon: "utility:apex",
-            URL: "/_ui/common/apex/debug/ApexCSIPage"
-        }
-    ]
+    shortcuts = SHORTCUTS
     
     @wire(getRecord, { recordId: '$csId', fields: ['Clicks_Saved__c.Id', 'Clicks_Saved__c.Count__c'] })
     clicksSaved;
