0115e2eccdea9753c697112c75c51ffc91ee902e
Ben <blearn5241@gmail.com>
2022-03-04 07:17:40 -0500

    fixed and made unlocked package

--- force-app/main/default/lwc/shortcutLink/shortcutLink.js
+++ force-app/main/default/lwc/shortcutLink/shortcutLink.js
@@ -1,19 +1,12 @@
 import { LightningElement, api } from 'lwc';
-import updateClicksCount from '@salesforce/apex/ShortcutButtonsController.updateClicksCount'
+
 
 export default class ShortcutButton extends LightningElement {
-    @api shortcutName = ''
-    @api shortcutIcon = ''
-    @api shortcutUrl = ''
-    error = undefined
+    @api shortcutName = '';
+    @api shortcutIcon = '';
+    @api shortcutUrl = '';
 
     updateCount() {
-        updateClicksCount({link: this.shortcutName})
-            .then(result => {
-                this.dispatchEvent(new CustomEvent('countupdate'))
-            })
-            .catch(error => {
-                this.error = error
-            })
+        this.dispatchEvent(new CustomEvent('countupdate'));
     }
 }
--- force-app/main/default/lwc/shortcuts/shortcuts.js
+++ force-app/main/default/lwc/shortcuts/shortcuts.js
@@ -1,52 +1,46 @@
-import { LightningElement, wire } from 'lwc'
-import { refreshApex } from '@salesforce/apex'
-import { getRecord } from 'lightning/uiRecordApi'
-import getClicksId from '@salesforce/apex/ShortcutButtonsController.getClicksId'
+import { LightningElement, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 
-const SHORTCUTS = [
-    {
-        Name: "Setup",
-        Icon: "utility:setup",
-        URL: "/lightning/setup/SetupOneHome/home"
-    },
-    {
-        Name: "Object Manager",
-        Icon: "utility:database",
-        URL: "/lightning/setup/ObjectManager/home"
-    },
-    {
-        Name: "Developer Console",
-        Icon: "utility:apex",
-        URL: "/_ui/common/apex/debug/ApexCSIPage"
-    }
-]
+import getClicksId from '@salesforce/apex/ShortcutsService.getClicksId';
+import getShortcuts from '@salesforce/apex/ShortcutsService.getShortcuts';
+import updateClicksCount from '@salesforce/apex/ShortcutsService.updateClicksCount';
 
 export default class Shortcuts extends LightningElement {
-    error = undefined
-    csId = ''
-    recordExists = false
-    shortcuts = SHORTCUTS
-    
-    @wire(getRecord, { recordId: '$csId', fields: ['Clicks_Saved__c.Id', 'Clicks_Saved__c.Count__c'] })
-    clicksSaved;
+    clicksIdResponse;
+    csId = '';
+    isLoading = true;
+    recordExists;
+    shortcuts;
 
     @wire(getClicksId)
-    wiredClickId({error, data}) {
+    parseClicksId(value) {
+        this.clicksIdResponse = value;
+        const {error, data} = this.clicksIdResponse;
+        
         if(data) {
             if(data[0]) {
-                this.csId = '' + data[0].Id
-                this.recordExists = true
+                this.csId = '' + data[0].Id;
+                this.recordExists = true;
             }
-            this.error = undefined
+
+            this.isLoading = false;
+
         } else if(error) {
-            this.error = error
+            console.error(error);
         }
     }
 
-    handleCountUpdate() {
-        refreshApex(this.wiredClickId)
-        if(this.csId) {
-            refreshApex(this.clicksSaved)
+    @wire(getShortcuts)
+    parseShortcuts({error, data}) {
+        if(data) {
+            this.shortcuts = data;
+        } else if(error) {
+            console.error(error);
         }
+    }
+
+    async handleCountUpdate() {
+        await updateClicksCount({link: this.shortcutName});
+        await refreshApex(this.clicksIdResponse);
     }
 }
